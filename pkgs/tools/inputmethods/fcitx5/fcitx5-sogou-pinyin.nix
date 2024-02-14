fe36692792005fa019b55fe481b7cb825d81d3e3
Kiara Grouwstra <kiara@bij1.org>
2024-02-14 21:57:18 +0100

    update sogou url to whatever their website had me make a request to

--- pkgs/tools/inputmethods/fcitx5/fcitx5-sogou-pinyin.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-sogou-pinyin.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation {
   name = "fcitx5-sogou-pinyin-${version}";
   src = fetchurl {
     name = "sogou-pinyin.deb";
-    url = "http://cdn2.ime.sogou.com/dl/index/1524572264/sogoupinyin_${version}_amd64.deb";
+    url = "https://ime-sec.gtimg.com/202402142154/3e8acc7b8894e2632d9081fb19185e7b/pc/dl/gzindex/1680521603/sogoupinyin_${version}_amd64.deb";
     sha256 = "3111af17a6abddd80b856aa9c1f579a137d69f3d735ead936ddb6e5f08b59f3b";
   };
   buildInputs = [ dpkg fcitx5 opencc lsb-release xprop ];
