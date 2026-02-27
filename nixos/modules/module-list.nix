9fd65cd143a18339b339891098448c4b4432629f
ibizaman <ibizaman@tiserbox.com>
2026-02-27 23:53:14 +0100

    hardcoded-secret: init with contract for secrets

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1976,6 +1976,7 @@
   ./tasks/stratis.nix
   ./tasks/swraid.nix
   ./tasks/trackpoint.nix
+  ./testing/hardcoded-secret.nix
   ./testing/service-runner.nix
   ./virtualisation/amazon-options.nix
   ./virtualisation/appvm.nix
