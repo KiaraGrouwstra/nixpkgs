280e0c629b7f1af0226d36626b81cddecb86dac3
ibizaman <ibizaman@tiserbox.com>
2026-02-27 23:53:14 +0100

    hardcoded-secret: init with contract for secrets
    This initial change demonstrates the concept of a contract:
    an expectation as verified by a test (here: `fileSecrets` test),
    that may be fulfilled by a provider (in our example here: `hardcoded-secret`).
    
    This simple example demonstrates that at their core, contracts do not
    technically require any machinery: without the test,
    our `hardcoded-secret` 'contract provider' module would still just work:
    
    This just uses existing NixOS machinery of options, types
    (with those of the abstract expectation in this change still left implicit:
    the interface used by the test to verify expected behavior), and tests.
    
    What this achieves is also just things the module system does:
    given the options, one may put info in, which may generate
    configuration, as well as lead to calculated options yielding
    information one may take back out again.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1990,6 +1990,7 @@
   ./tasks/stratis.nix
   ./tasks/swraid.nix
   ./tasks/trackpoint.nix
+  ./testing/hardcoded-secret.nix
   ./testing/service-runner.nix
   ./virtualisation/amazon-options.nix
   ./virtualisation/appvm.nix
