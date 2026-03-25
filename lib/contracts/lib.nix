58b579ffe6b54bfbdd7197a45a78b2da15260230
cinereal <cinereal@riseup.net>
2026-03-25 19:30:08 +0100

    lib.contracts: improve docs on getInputs
    Signed-off-by: cinereal <cinereal@riseup.net>

--- lib/contracts/lib.nix
+++ lib/contracts/lib.nix
@@ -2,6 +2,13 @@
 {
   /**
     Get the request inputs for a given contract.
+    A provider may use this to assign to its contract instances:
+
+    ```nix
+    services.<provider>.<contract> = lib.contract.getInputs config.contracts.<contract>;
+    ```
+
+    Filtering request content this way helps prevent an infinite recursion through `output`s.
 
     # Inputs
 
@@ -12,7 +19,7 @@
     # Type
 
     ```
-    getInputs :: Contract -> attrsOf (attrsOf attrs)
+    lib.contract.getInputs :: Contract -> attrsOf (attrsOf attrs)
     ```
   */
   getInputs = contract: lib.mapAttrs (
