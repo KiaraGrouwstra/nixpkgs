1bacdacb93e0005dedaa7eebda066363e4a732af
cinereal <cinereal@riseup.net>
2026-03-29 00:11:38 +0100

    rename old `requests` to `invoke` to distinguish from used nouns `request` and `requests` , open to suggestions...
    Signed-off-by: cinereal <cinereal@riseup.net>

--- nixos/modules/system/service/nixos-contracts-bridge.nix
+++ nixos/modules/system/service/nixos-contracts-bridge.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 {
   contracts = lib.mapAttrs (contractType: _: {
-    requests = lib.mkMerge (
+    invoke = lib.mkMerge (
       (import ./portable/lib.nix { inherit lib; }).flattenMapServicesConfigToList (
         _: service: lib.toList (service.contractRequests.${contractType} or { })
       ) [ ] config.system
