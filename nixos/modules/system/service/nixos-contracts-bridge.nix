31a711b0d9f01de25330ca3e4e2c510557a37348
cinereal <cinereal@riseup.net>
2026-03-31 17:14:09 +0200

    add bridge for contract providers that are modular services
    Signed-off-by: cinereal <cinereal@riseup.net>

--- nixos/modules/system/service/nixos-contracts-bridge.nix
+++ nixos/modules/system/service/nixos-contracts-bridge.nix
@@ -1,9 +1,19 @@
 { config, lib, ... }:
+let
+  portable-lib = import ./portable/lib.nix { inherit lib; };
+in
 {
   contracts = lib.mapAttrs (contractType: _: {
     want = lib.mkMerge (
-      (import ./portable/lib.nix { inherit lib; }).flattenMapServicesConfigToList (
+      portable-lib.flattenMapServicesConfigToList (
         _: service: lib.toList (service.contractRequests.${contractType} or { })
+      ) [ ] config.system
+    );
+    providers = lib.mkMerge (
+      portable-lib.flattenMapServicesConfigToList (
+        loc: service:
+          lib.optional (loc != [ ] && service.contractProviders ? ${contractType})
+            { ${lib.last loc} = service.contractProviders.${contractType}; }
       ) [ ] config.system
     );
   }) config.contractTypes;
