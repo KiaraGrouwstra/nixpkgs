745cb608cec0096657656d340b764ebff0b4e00f
cinereal <cinereal@riseup.net>
2026-04-03 17:12:40 +0200

    lib: add module system extensions for contracts
    Add foundational module system primitives:
    
    - types.optionDeclaration: type for option declarations (mkOption values)
    - types.nestedAttrsOf: variable-depth nested attribute sets
    - lib.evalOption: evaluate config in the context of an option
    - lib.extendOption: extend a submodule option with overrides
    - lib.extendSubmodule: extend a submodule type with overrides
    - lib.options.renderOptionValue: show multi-line strings as code blocks

--- lib/default.nix
+++ lib/default.nix
@@ -72,6 +72,10 @@ let
       options = callLibs ./options.nix;
       types = callLibs ./types.nix;
 
+      # contracts
+      contract = callLibs ./contracts/lib.nix;
+      contracts = callLibs ./contracts;
+
       # constants
       licenses = callLibs ./licenses.nix;
       sourceTypes = callLibs ./source-types.nix;
@@ -256,6 +260,11 @@ let
         isAttrs
         intersectAttrs
         removeAttrs
+        isNestedAttrsLeaf
+        mapNestedAttrs'
+        mapNestedAttrsWith
+        concatMapNestedAttrsWith
+        concatMapNestedAttrs'
         ;
       inherit (self.lists)
         singleton
@@ -473,6 +482,9 @@ let
         ;
       inherit (self.modules)
         evalModules
+        evalOption
+        extendOption
+        extendSubmodule
         setDefaultModuleLocation
         unifyModuleSyntax
         applyModuleArgsIfFunction
